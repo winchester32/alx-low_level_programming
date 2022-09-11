cca04ebe62438976ce35013115bafee4716c6723
winchester32 <wnmburuk@gmail.com>
2022-09-11 07:12:39 -0700

    same

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		(printf("%d", i));
+	}
+	putchar('\n');
+	return (0);
+}
